1fe662ab31e69338cddddc259ec35beea20ace25
joeysola <joeysolak@gmail.com>
2016-11-12 20:06:41 -0500

    Update to Add <String>

--- BinarySearch/src/binarysearch/BSDemo.java
+++ BinarySearch/src/binarysearch/BSDemo.java
@@ -1,43 +1,57 @@
 /*
  * Name: Joey Solak
- * Date:
+ * Date: 11/12/16
  * Class: CIT-130
- * Purpose Of The File:
+ * Purpose Of The File: Test a templated binary search.
  */
 package binarysearch;
+
+import java.util.Arrays;
 
 /**
  *
  * @author Joey Solak
+ * Used Display 11.8 from Absolute Java book as a reference.
  */
 public class BSDemo {
     
     public static void main(String[] args){
-        
     
-    int[] a = {-2, 0, 2, 4, 6, 8, 10, 12, 14, 16};
-    int finalIndex = 9;
+    //Intialize BinarySearch
+    BinarySearch bs = new BinarySearch();
     
-    System.out.println("Array contains");
-    
-    for (int i = 0; i < a.length; i++){
-        System.out.print(a[i] + " ");
-    }
-    System.out.print("\n\n");
-    
+    //Set Variables and Arrays
+    int key;
     int result;
-    for (int key = -3; key < 5; key++){
-        result = BinarySearch.search(a, 0, finalIndex, key);
-        if (result >= 0){
-            System.out.println(key + " is at index " + result);
-        } else {
-            System.out.println(key + " is not in the array.");
-        }
+    Integer[] a = {0, 1, 4, 5};
+    String[] b = {"A", "AB", "ABC", "ABCD", "ABCDE", "ABCDEF"};
+    
+    //Test <Integer>
+    System.out.print("Test <Integer> is:");
+    System.out.println(Arrays.toString(a));
+    
+    //Use BinarySearch for <Integer>
+    for(key = -2; key < 3; key++){
+        result = bs.<Integer>Bsearch(a, 0, 4, key);
+        bs.toString(Integer.toString(key), result);
     }
     
-    for (int i = 0; i < a.length; i++){
-        System.out.print(a[i] + " ");
-    }
+    //Test <String>
+    System.out.print("\nTest <String> is:");
+    System.out.println(Arrays.toString(b));
     
+    //Display parts of the <String> Array using the BinarySearch
+    result = bs.<String>Bsearch(b, 0, 10, "AB");
+    bs.toString("AB", result);
+    
+    result = bs.<String>Bsearch(b, 0, 10, "ABC");
+    bs.toString("ABC", result);
+    
+    result = bs.<String>Bsearch(b, 0, 10, "A");
+    bs.toString("A", result);
+    
+
+    
+
 }
 }
--- BinarySearch/src/binarysearch/BinarySearch.java
+++ BinarySearch/src/binarysearch/BinarySearch.java
@@ -1,20 +1,22 @@
 /*
  * Name: Joey Solak
- * Date:
+ * Date: 11/12/16
  * Class: CIT-130
- * Purpose Of The File:
+ * Purpose Of The File: Implement a templated binary search.
  */
 package binarysearch;
 
 /**
  *
  * @author Joey Solak
+ * Used Display 11.6 from the Absolute Java book as a starting point.
  */
 public class BinarySearch {
 
-    public static <T extends Comparable> int search(T[] a, int first, int last, T key){
+    @SuppressWarnings("unchecked")
+    public static <T extends Comparable> int Bsearch(T[] a, int first, int last, T key){
         
-        int result = 0;
+        int result;
         
         if (first > last){
             result = -1;
@@ -26,17 +28,34 @@ public class BinarySearch {
                 result = mid;
             }
             else if(key.compareTo(a[mid]) < 0){
-                result = search(a, first, mid - 1, key);
+                result = Bsearch(a, first, mid - 1, key);
             }
              else if (key.compareTo(a[mid]) > 0){
-                result = search(a, mid + 1, last, key);
-            
-     
-        }
-                   
+                result = Bsearch(a, mid + 1, last, key);
+            }
+             else{
+                System.out.println("Error");
+                result = -1;
+             }     
   
     }
         return result; 
     
  }
+
+    
+    public static void toString(String key, int result) {
+     
+        if(result == -1){
+            System.out.println(key + " is not in the array.");
+        } else {
+            System.out.println(key + " is at index " + result);
+        }
+            
+        
+        
+    }
+    
+    
+    
 }
